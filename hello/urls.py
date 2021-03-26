488b63f4b1191498afdf34f8462294339b0f6e50
DrBubbles42 <22nickfox@gmail.com>
2021-03-26 14:16:34 -0400

    connected to reddit and display the top post in the hot section for r/stocks on the /reddit page.

--- hello/urls.py
+++ hello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("reddit", views.reddit, name="reddit"),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,26 @@
 from django.http import HttpResponse
+#rom django.http import JsonResponse
+
+#import jason
+import praw
+import logging
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def reddit(request):
+    #debug
+    out = ""
+
+    #connect to reddit
+    reddit = praw.Reddit(
+        client_id = 'TjJjmgfWJrHi6g',
+        client_secret = 'lpOjNJ9DQATig-FQWh0pDzcFQEAU2Q',
+        user_agent = 'databaseproject'
+    )
+
+    #get subreddit
+    for submission in reddit.subreddit('stocks').hot(limit = 1):
+        out = out + submission.title + " "
+
+    return HttpResponse(out)
