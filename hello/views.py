53c6a758fde8ac431687a44787de3e87208b2d82
DrBubbles42 <22nickfox@gmail.com>
2021-03-27 23:45:04 -0400

    added yfinance. It currently scrapes the top 50 hot in r/stocks. added regex matcher and validation if the matched ticker is a stock or not.

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
-#rom django.http import JsonResponse
+from django.http import JsonResponse
 
-#import jason
+import json
 import praw
+import re
+import yfinance as yf
 import logging
 
 def home(request):
@@ -19,8 +21,29 @@ def reddit(request):
         user_agent = 'databaseproject'
     )
 
-    #get subreddit
-    for submission in reddit.subreddit('stocks').hot(limit = 1):
-        out = out + submission.title + " "
+    tickers = []
+    errortic = yf.Ticker("")
 
-    return HttpResponse(out)
+    #get subreddit and scrape for tickers. place possible tickeres in 2d array
+    for submission in reddit.subreddit('stocks').hot(limit = 50):
+        row = re.findall(r'(?<![a-zA-Z])\^*([A-Z]{1,5})(?![a-zA-Z])', submission.title)
+        if row:
+            tickers.append(row)
+
+    #read array and check if its a valid ticker with yf (this can be optimized by builting a heap or binary tree of already found tickers)
+    for r in tickers:
+        for c in r:
+            print(c)
+            ticker = yf.Ticker(c)
+            try:
+                if (errortic.calendar == ticker.calendar):
+                    print("Not a stock")
+            except:
+                #on existing ticker print ticker obj. Add to database here
+                print(ticker)
+
+
+
+    return HttpResponse("check terminal")
+
+    #return HttpResponse(out)
