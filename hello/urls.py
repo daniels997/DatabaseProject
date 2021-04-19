885dc63d718ae2fe8d0c8175196999e9187cd572
Benjamin Rodriguez <brodriguez3@ufl.edu>
2021-04-19 14:25:38 -0400

    working on views and stuff
    views are in .venv/Lib\site-packages/django/contrib/admin/templates

--- hello/urls.py
+++ hello/urls.py
@@ -3,4 +3,11 @@ from hello import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("query1", views.query1, name="query1"),
+    path("query2", views.query2, name="query2"),
+    path("query3", views.query3, name="query3"),
+    path("query4", views.query4, name="query4"),
+    path("query5", views.query5, name="query5"),
+    path("query6", views.query6, name="query6"),
+    path("query7", views.query7, name="query7")
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,73 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("BUY GME!")
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query1(request):
+    return render(
+        request,
+        'query1.html',
+        #context={x},
+    )
+
+def query2(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query3(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query4(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query5(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query6(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
+
+def query7(request):
+    #return HttpResponse("buy gme i guess")
+    return render(
+        request,
+        'index.html',
+        {
+        }
+    )
