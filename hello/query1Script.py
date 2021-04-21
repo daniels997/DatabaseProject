7e14aea04b145757a4b216e2b947564ce7fad1fb
Benjamin Rodriguez <brodriguez3@ufl.edu>
2021-04-21 02:20:23 -0400

    progress tm
    lol

--- hello/query1Script.py
+++ hello/query1Script.py
@@ -1,6 +1,13 @@
-
+import cx_Oracle
 
 def query1Script(x, date1, date2, y, z):
+
+    date1 = str(date1) + ' 23:59:59'
+    date2 = str(date2) + ' 23:59:59'
+
+    for row in cur.execute("select sq.Stock_name from ( select Stock.Stock_name, count(t_mentions.tweet), count(r_mentions.reddit_post) from stock join instance_of_stock on stock.ticker = instance_of_stock.stock join stock_instance on stock_instance.instance_ID = instance_of_stock.stock_instance join t_mentions on stock.ticker = t_mentions.stock join r_mentions on stock.ticker = r_mentions.stock where stock_volume >  :x and Instance_Date between to_TIMESTAMP(:date1, 'YYYY-MM-DD HH24:MI:SS.FF') AND to_TIMESTAMP(:date2, 'YYYY-MM-DD HH24:MI:SS.FF') group by Stock.Stock_name having count(t_mentions.tweet) >= :y and count(r_mentions.reddit_post) >= :z) sq", [x, date1, date2, y, z]):
+        print(row)
+
     print(x, ' ', date1, ' ', date2, ' ', y, ' ', z)
     #query database with these things
     #display data
--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from .forms import query1Form, query2Form, query3Form, query4Form, query5Form, query6Form, query7Form
-from .query1Script import query1Script
+#from .query1Script import query1Script
 from .query2Script import query2Script
 from .query3Script import query3Script
 from .query4Script import query4Script
@@ -11,6 +11,45 @@ from .query5Script import query5Script
 from .query6Script import query6Script
 from .query7Script import query7Script
 
+#import plotly.express as px
+import pandas as pd
+from IPython.display import HTML
+
+import cx_Oracle
+
+cx_Oracle.init_oracle_client(lib_dir=r"C:\\Program Files\\instantclient_19_10")
+CONN_INFO = {
+            'host': 'oracle.cise.ufl.edu',
+            'port': 1521,
+            'user': 'fox.nbrian',
+            'psw': 'Tga20379',
+            'sid': 'orcl'
+            }
+CONN_STR = '{user}/{psw}@{host}:{port}/{sid}'.format(**CONN_INFO)
+con = cx_Oracle.connect(CONN_STR)
+cur = con.cursor()
+cur.execute("alter session set NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF'")
+
+def query1Script(x, date1, date2, y, z):
+
+    date1 = str(date1) + ' 23:59:59'
+    date2 = str(date2) + ' 23:59:59'
+
+    cur.execute("select sq.Stock_name from ( select Stock.Stock_name, count(t_mentions.tweet), count(r_mentions.reddit_post) from stock join instance_of_stock on stock.ticker = instance_of_stock.stock join stock_instance on stock_instance.instance_ID = instance_of_stock.stock_instance join t_mentions on stock.ticker = t_mentions.stock join r_mentions on stock.ticker = r_mentions.stock where stock_volume >  :x and Instance_Date between to_TIMESTAMP(:date1, 'YYYY-MM-DD HH24:MI:SS.FF') AND to_TIMESTAMP(:date2, 'YYYY-MM-DD HH24:MI:SS.FF') group by Stock.Stock_name having count(t_mentions.tweet) >= :y and count(r_mentions.reddit_post) >= :z) sq", [x, date1, date2, y, z])
+    array = []
+
+    for row in cur.fetchall():
+        array.append(row[0])
+
+    print(array)
+    print(x, ' ', date1, ' ', date2, ' ', y, ' ', z)
+
+    df = pd.DataFrame({"Stock Name": array})
+    print("Original DataFrame :")
+    result = df.to_html()
+    print(result)
+
+    return array
 
 def home(request):
     #return HttpResponse("buy gme i guess")
@@ -25,7 +64,7 @@ def query1(request):
     if request.method == 'POST':
         form = query1Form(request.POST)
         if form.is_valid():
-            query1Script(form.cleaned_data['x'], form.cleaned_data['date1'], form.cleaned_data['date2'], form.cleaned_data['y'], form.cleaned_data['z'])
+            array = query1Script(form.cleaned_data['x'], form.cleaned_data['date1'], form.cleaned_data['date2'], form.cleaned_data['y'], form.cleaned_data['z'])
     else:
         form = query1Form()
 
